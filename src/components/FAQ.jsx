598453221ebea916ced801f1738c433c5b210ba8
jbnzaaa <36443558+jbnzaaa@users.noreply.github.com>
2022-09-05 11:16:22 +0800

    update FAQ dropdowns

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {BsPlusLg} from 'react-icons/bs'
+import {AiOutlineMinus} from 'react-icons/ai'
 import Fade from 'react-reveal/Fade'
 
 function FAQ() {
+  const [showFAQ1, setShowFAQ1] = useState(false)
+  const [showFAQ2, setShowFAQ2] = useState(false)
+  const [showFAQ3, setShowFAQ3] = useState(false)
+  const [showFAQ4, setShowFAQ4] = useState(false)
+  const [showFAQ5, setShowFAQ5] = useState(false)
+  const [showFAQ6, setShowFAQ6] = useState(false)
+
   return (
     <>
       <div id="FAQ">
@@ -15,54 +23,98 @@ function FAQ() {
           </section>
           {/* Guidelines */}
           <section className='my-10 text-sm font-montserrat'>
-            <ul className="grid grid-rows-6 gap-3 sm:grid-cols-1 gap-1">
-              <li className='flex justify-between py-3'>
-                <Fade bottom>
-                  <p className='text-[18px] sm:text-[14px] w-[170px] md:text-[16px] w-[280px]'>What is the first step of the home buying process?</p>
-                  <button className='bg-transparent'>
-                    <BsPlusLg className='text-[18px] text-[14px]'/>
-                  </button>
-                </Fade>
+            <ul className="grid grid-rows-1 gap-3 sm:grid-cols-1 gap-1">
+              <li>
+                <div className="flex justify-between py-3">
+                  <Fade bottom>
+                    <p className='text-[18px] sm:text-[14px] w-auto'>What is the first step of the home buying process?</p>
+                    <button className='bg-transparent pl-3'>
+                      {showFAQ1 ? <AiOutlineMinus className='text-[18px] text-[14px]' onClick={() => setShowFAQ1(false)}/> : <BsPlusLg className='text-[18px] text-[14px]' onClick={() => setShowFAQ1(true)}/>}
+                    </button>
+                  </Fade>
+                </div>
+                {showFAQ1 ? (
+                  <p className='flex items-center text-black sm:text-xs w-auto md:text-sm'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi voluptates tempora nostrum debitis! Tempore.
+                  </p>
+                ) : null}
               </li>
-              <li className='flex justify-between py-3'>
-                <Fade bottom delay={40}>
-                  <p className='text-[18px] sm:text-[14px] w-[170px] md:text-[16px] w-[400px]'>How long does it take to buy a home?</p>
-                  <button className='bg-transparent'>
-                    <BsPlusLg className='text-[18px] text-[14px]'/>
-                  </button>
-                </Fade>
+              <li>
+                <div className="flex justify-between py-3">
+                  <Fade bottom>
+                    <p className='text-[18px] sm:text-[14px] w-auto'>How long does it take to buy a home?</p>
+                    <button className='bg-transparent pl-3'>
+                      {showFAQ2 ? <AiOutlineMinus className='text-[18px] text-[14px]' onClick={() => setShowFAQ2(false)}/> : <BsPlusLg className='text-[18px] text-[14px]' onClick={() => setShowFAQ2(true)}/>}
+                    </button>
+                  </Fade>
+                </div>
+                {showFAQ2 ? (
+                  <p className='flex items-center text-black sm:text-xs w-auto md:text-sm'>
+                    Lorem ipsum dolor sit amet consectetur accusamus maiores perspiciatis modi blanditiis atque sint. Tenetur, aut vitae!
+                  </p>
+                ) : null}
               </li>
-              <li className='flex justify-between py-3'>
-                <Fade bottom delay={80}>
-                  <p className='text-[18px] sm:text-[14px] w-[170px] md:text-[16px] w-[400px]'>How much do I have to pay an agent to help me buy a house?</p>
-                  <button className='bg-transparent'>
-                    <BsPlusLg className='text-[18px] text-[14px]'/>
-                  </button>
-                </Fade>
+              <li>
+                <div className="flex justify-between py-3">
+                  <Fade bottom>
+                    <p className='text-[18px] sm:text-[14px] w-auto'>How much do I have to pay an agent to help me buy a house?</p>
+                    <button className='bg-transparent pl-3'>
+                      {showFAQ3 ? <AiOutlineMinus className='text-[18px] text-[14px]' onClick={() => setShowFAQ3(false)}/> : <BsPlusLg className='text-[18px] text-[14px]' onClick={() => setShowFAQ3(true)}/>}
+                    </button>
+                  </Fade>
+                </div>
+                {showFAQ3 ? (
+                  <p className='flex items-center text-black sm:text-xs w-auto md:text-sm'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi voluptates tempora nostrum debitis! Tempore, 
+                    quo deserunt quidem at.
+                  </p>
+                ) : null}
               </li>
-              <li className='flex justify-between py-3'>
-                <Fade bottom delay={120}>
-                  <p className='text-[18px] sm:text-[14px] w-[170px] md:text-[16px] w-[400px]'>What kind of credit score do I need to buy a home?</p>
-                  <button className='bg-transparent'>
-                    <BsPlusLg className='text-[18px] text-[14px]'/>
-                  </button>
-                </Fade>
+              <li>
+                <div className="flex justify-between py-3">
+                  <Fade bottom>
+                    <p className='text-[18px] sm:text-[14px] w-auto'>What kind of credit score do I need to buy a home?</p>
+                    <button className='bg-transparent pl-3'>
+                      {showFAQ4 ? <AiOutlineMinus className='text-[18px] text-[14px]' onClick={() => setShowFAQ4(false)}/> : <BsPlusLg className='text-[18px] text-[14px]' onClick={() => setShowFAQ4(true)}/>}
+                    </button>
+                  </Fade>
+                </div>
+                {showFAQ4 ? (
+                  <p className='flex items-center text-black sm:text-xs w-auto md:text-sm'>
+                    Animi voluptates tempora nostrum debitis! Tempore, quo deserunt quidem at, nemo reprehenderit accusamus maiores perspiciatis modi blanditiis atque sint.
+                  </p>
+                ) : null}
               </li>
-              <li className='flex justify-between py-3'>
-                <Fade bottom delay={160}>
-                  <p className='text-[18px] sm:text-[14px] w-[170px] md:text-[16px] w-[400px]'>How much do I need for a down payment?</p>
-                  <button className='bg-transparent'>
-                    <BsPlusLg className='text-[18px] text-[14px]'/>
-                  </button>
-                </Fade>
+              <li>
+                <div className="flex justify-between py-3">
+                  <Fade bottom>
+                    <p className='text-[18px] sm:text-[14px] w-auto'>How much do I need for a down payment?</p>
+                    <button className='bg-transparent pl-3'>
+                      {showFAQ5 ? <AiOutlineMinus className='text-[18px] text-[14px]' onClick={() => setShowFAQ5(false)}/> : <BsPlusLg className='text-[18px] text-[14px]' onClick={() => setShowFAQ5(true)}/>}
+                    </button>
+                  </Fade>
+                </div>
+                {showFAQ5 ? (
+                  <p className='flex items-center text-black sm:text-xs w-auto md:text-sm'>
+                    Nemo reprehenderit accusamus maiores perspiciatis modi blanditiis atque sint. Tenetur, aut vitae!
+                  </p>
+                ) : null}
               </li>
-              <li className='flex justify-between py-3'>
-                <Fade bottom delay={210}>
-                  <p className='text-[18px] sm:text-[14px] w-[170px] md:text-[16px] w-[400px]'>How long can the seller take to respond to my offer?</p>
-                  <button className='bg-transparent'>
-                    <BsPlusLg className='text-[18px] text-[14px]'/>
-                  </button>
-                </Fade>
+              <li>
+                <div className="flex justify-between py-3">
+                  <Fade bottom>
+                    <p className='text-[18px] sm:text-[14px] w-auto'>How long can the seller take to respond to my offer?</p>
+                    <button className='bg-transparent pl-3'>
+                      {showFAQ6 ? <AiOutlineMinus className='text-[18px] text-[14px]' onClick={() => setShowFAQ6(false)}/> : <BsPlusLg className='text-[18px] text-[14px]' onClick={() => setShowFAQ6(true)}/>}
+                    </button>
+                  </Fade>
+                </div>
+                {showFAQ6 ? (
+                  <p className='flex items-center text-black sm:text-xs w-auto md:text-sm'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi voluptates tempora nostrum debitis! Tempore, 
+                    quo deserunt quidem at, nemo reprehenderit accusamus maiores.
+                  </p>
+                ) : null}
               </li>
             </ul>
           </section>
